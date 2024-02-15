c63a65795ddbcf27b5f242920a7f46ccb9cc4cb1
Rotem Grimberg <rotemgrim@gmail.com>
2024-02-15 14:19:03 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,30 @@
+const config = {
+    
+    prompt: {
+        name: `<c class="brightRed">guest</c>`,
+        at: `<c class="white"> @ </c>`,
+        host: `<c class="green">great.engineer</c> `,
+        separator: `<c class="white">$</c>`,
+    },
+    
+    welcome: `
+
+<c class="brightRed">  ⠀⠀⠀⢠⡀                         </c>
+<c class="brightRed"> ⠀⠀⠀⠀⠈⢿⣦⡀                       </c>
+<c class="brightRed">⠀⠀⠀⢦⣄⠰⣄⡛⢿⣷⣦⣤⣀⡀               </c>
+<c class="brightRed">⠀⠀⠈⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣷⣦⣀            </c>
+<c class="brightRed">⠀⢀⣴⣿⣿⣿⣿⣿⣿⣻⣿⣿⣿⣿⣌⡻⣿⣄         </c>
+<c class="brightRed">⢀⣾⣿⣿⡿⢚⣿⡿⠟⠙⢿⣿⣿⣿⡟⠻⢿⣿⣷⣶⣆     </c>
+<c class="brightRed">⠀⣼⣿⡿⣫⣾⡖⠀⠐⣿⠗⠀⠉⠻⣿⣷⠀⠀⠈⠙⢿⡏     </c>
+<c class="brightRed">⠀⣿⣿⣵⣿⡟⢴⣦⣤⠙⠀⠀⠀⢀⣼⣿⣆⡀⠀⠀⠘⠀⠀⠀   </c>
+<c class="brightRed">⠀⡿⢻⣿⣿⡇⡌⢻⣿⡀⠀⠀⠀⠀⠈⠉⠉⠁       </c><c class="foreground pixelated">[time]</c>
+<c class="brightRed">⠀⠇⠸⣿⣿⡇⣧⡀⠈⠓      </c>     ___       __              _____    _       __               
+<c class="brightRed">  ⠀⢻⣿⣇⢻⣷⡴⢦⣤⣀    </c>    / _ \\___  / /____ __ _    / ___/___(_)_ _  / /  ___ _______ _
+<c class="brightRed">  ⠀⠀⠙⣿⡀⠻⠿⢶⣤⣀⣀   </c>   / , _/ _ \\/ __/ -_)  ' \\  / (_ / __/ /  ' \\/ _ \\/ -_) __/ _ \`/
+<c class="brightRed">  ⠀⠀⠀⠈⠓⠄⠀⠀⠀⠀⠉⠁  </c>   /_/|_|\\___/\\__/\\__/_/_/_/  \\___/_/ /_/_/_/_/_.__/\\__/_/  \\_, / 
+<c class="brightBlack">~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~</c> /___/  
+<c class="blue pixelated">Welcome to my interactive web terminal.</c>
+<c class="brightGreen">For a list of available commands, type <c class="cyan glow">'help'</c>.</c>
+<br/>`,
+
+};
